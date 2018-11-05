c91e8f2cc7fa425fa2a1a61b59c6b3b054a9e2c6
supersilent <shrikethrush@gmail.com>
2018-11-05 14:46:44 -0800

    Tyler and Shota stringifyJSON recursion working version

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,51 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'function' || obj === undefined){
+    return undefined;
+  }
+  
+  if (obj === null){
+    return 'null';
+  }
+  
+  let string='';
+  if (typeof obj==='string'){
+    return '"'+obj+'"';
+  }  
+
+  if (typeof obj==='number'|| typeof obj==='boolean'){
+    return String(obj);
+  }
+  
+  if (Array.isArray(obj)){
+    let array=[];
+    
+    for (let e of obj){
+      array.push(stringifyJSON(e));
+    }
+    string=array.join(',');
+
+    return '['+ string + ']';        
+  }
+  
+  if (typeof obj==='object'){
+    for (let key in obj){
+      if (typeof obj[key] === 'function'){
+        return '{}';
+      }
+    }  
+    
+    string+='{'
+    
+    let array=[];
+    for (let key in obj){
+      array.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    string+=array.join(',')+ '}';
+    return string;        
+  }
+  
+  
+
 };
