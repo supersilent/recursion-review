d88b7a8b5be653ca903baeed2a1c084f611cb055
supersilent <shrikethrush@gmail.com>
2018-11-05 15:56:50 -0800

    In progress for getElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
+  var ElementArr = [];
+    
+  let currentNode = document.body;
   // your code here
+  let addElements = function (node) {
+    if (node.classList.contains(className)) {
+      ElementArr.push(node);
+    }
+  };
+  addElements(currentNode);
+  
+  while (currentNode.childNodes) {
+    for (let e of currentNode.childNodes) {
+      addElements(e);
+    }
+    
+  }
+  return ElementArr;
+
+
 };
